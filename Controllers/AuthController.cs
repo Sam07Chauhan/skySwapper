513a945f294a0051dc03ea99b49fedac66e6acac
Shivam Chauhan <shivam07chauhan@gmail.com>
2024-09-10 13:40:18 +0530

    Initial commit for .NET Core API project skyWrapper

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace skySwapper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public AuthController(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        public Microsoft.Extensions.Logging.ILogger Logger { get; }
+    }
+}
